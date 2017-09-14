0c94135564fd806455270649f0707e51e7f9b136
Etienne Martin <github@emartin.ca>
2017-09-14 16:23:53 -0400

    Update webProxyDetector.php

--- webProxyDetector.php
+++ webProxyDetector.php
@@ -1,6 +1,7 @@
 <?php 
 
-$expectedOrigin = "http://example.com";
+$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos($_SERVER["SERVER_PROTOCOL"],"/")));
+$expectedOrigin = $protocol."://".htmlspecialchars($_SERVER["HTTP_HOST"]);
 	
 $corsOrigin = isset($_SERVER["HTTP_ORIGIN"]) ? htmlspecialchars($_SERVER["HTTP_ORIGIN"]) : "";
 $ajaxOrigin = isset($_SERVER["HTTP_X_CLIENT_ORIGIN"]) ? htmlspecialchars($_SERVER["HTTP_X_CLIENT_ORIGIN"]) : "";
@@ -21,10 +22,10 @@ if( $queryOrigin !== $expectedOrigin ){
 returnResponse(array("isProxy" => false, "proxy" => ""));
 
 function returnResponse($response, $responseCode = 200){
-    header('Access-Control-Allow-Origin: *');  
+    header("Access-Control-Allow-Origin: *");  
     header("Access-Control-Allow-Headers: x-client-origin");
-    header('X-PHP-Response-Code: '.$responseCode, true, $responseCode);
-    header('Content-Type: application/json; charset=utf-8');
+    header("X-PHP-Response-Code: ".$responseCode, true, $responseCode);
+    header("Content-Type: application/json; charset=utf-8");
     die(stripslashes(json_encode($response)));
 }
 	
